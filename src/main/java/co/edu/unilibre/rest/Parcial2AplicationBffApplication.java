f71d9890e8bb8b83d6c9f3d8db054a137d99a818
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2019-10-16 20:13:07 -0500

    Creacion

---
+++ src/main/java/co/edu/unilibre/rest/Parcial2AplicationBffApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unilibre.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parcial2AplicationBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parcial2AplicationBffApplication.class, args);
+	}
+
+}
